54eab097d8d5699e59a04c2ca7092e85329bcd82
Max <max_rickers@hotmail.de>
2022-11-24 11:00:53 +0100

    CalulateAnnuityController implemented

--- backend/pkg/controllers/annuity_controller.go
+++ backend/pkg/controllers/annuity_controller.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"kreditrechner/pkg/annuity"
 )
 
 type Logger interface {
@@ -11,8 +16,37 @@ type Logger interface {
 	Error(v ...any)
 }
 
+type HttpError struct {
+	Message string
+	Code    int
+}
+
+func (e *HttpError) Error() string {
+	return e.Message
+}
+
+type AnnuityRequest struct {
+	Creditsum                  uint    `json:"creditsum"`
+	Runtime                    uint    `json:"runtime"`
+	Interest_rate              float64 `json:"interest_rate"`
+	Initial_repayment_rate     float64 `json:"initial_repayment_rate"`
+	Unscheduled_repayment_rate float64 `json:"unscheduled_repayment_rate"`
+}
+
+type AnnuityResult struct {
+	Year                  uint `json:"year"`
+	Residual_dept         uint `json:"residual_dept"`
+	Interest              uint `json:"interest"`
+	Repayment             uint `json:"repayment"`
+	Unscheduled_repayment uint `json:"unscheduled_repayment"`
+	Annuity               uint `json:"annuity"`
+}
+
+type AnnuityResponse struct {
+	Results []AnnuityResult
+}
+
 func CalculateAnnuityController(logger Logger) http.Handler {
-	logger.Info("Hello from logging handler: %s", "Hi")
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		calculateAnnuityHandler(w, r, logger)
 	}
@@ -22,14 +56,92 @@ func CalculateAnnuityController(logger Logger) http.Handler {
 func calculateAnnuityHandler(w http.ResponseWriter, r *http.Request, logger Logger) {
 	if r.Method != http.MethodPost {
 		logger.Error("CalculateAnnuityHandler error: want post methode got %s", r.Method)
-		http.Error(w, "Method Not Allowed", 404)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// get json body
+	body, err := readHttpRequestBody(r, logger)
+
+	if err != nil {
+		code := err.(*HttpError).Code
+		http.Error(w, err.Error(), code)
+	}
+
+	// convert from json to annuity.Request
+	var annuity_request AnnuityRequest
+
+	err = json.Unmarshal(body, &annuity_request)
+
+	if err != nil {
+		logger.Error("Could not unmarshal body")
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	response, err := requestAnnuity(annuity_request)
+
+	if err != nil {
+		logger.Error("Calculate Annuity failed: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// get json
-	// logger.Info("Remote address: %s", r.RemoteAddr)
-	// logger.Info("HEADER:")
-	// for key, value := range r.Header {
-	// 	logger.Info("%s: %s", key, value)
-	// }
+	json_response, err := json.Marshal(response)
+
+	if err != nil {
+		logger.Error("Marshal json failed: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", string(json_response))
+}
+
+func readHttpRequestBody(r *http.Request, logger Logger) ([]byte, error) {
+	if r.ContentLength <= 0 {
+		logger.Error("No body available")
+		return nil, &HttpError{"Bad Request", http.StatusBadRequest}
+	}
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		logger.Error("Could not read request body: %s", err)
+		return nil, &HttpError{"Internal Server Error", http.StatusInternalServerError}
+	}
+	return body, nil
+}
+
+func requestAnnuity(request AnnuityRequest) (AnnuityResponse, error) {
+	annuity_request := annuity.NewRequest()
+	annuity_request.Creditsum = request.Creditsum
+	annuity_request.Initial_repayment_rate = request.Initial_repayment_rate
+	annuity_request.Interest_rate = request.Interest_rate
+	annuity_request.Runtime = request.Runtime
+	annuity_request.Unscheduled_repayment_rate = request.Unscheduled_repayment_rate
+
+	response, err := annuity.CalculateAnnuity(annuity_request)
+
+	if err != nil {
+		return AnnuityResponse{}, err
+	}
+	return convertResponse(response), nil
+}
+
+func convertResponse(response annuity.Response) AnnuityResponse {
+	var annuity_response AnnuityResponse
+
+	for _, result := range response.Results {
+		var annuity_result AnnuityResult
+
+		annuity_result.Annuity = result.Annuity
+		annuity_result.Interest = result.Interest
+		annuity_result.Repayment = result.Repayment
+		annuity_result.Residual_dept = result.Residual_dept
+		annuity_result.Unscheduled_repayment = result.Unscheduled_repayment
+		annuity_result.Year = result.Year
+
+		annuity_response.Results = append(annuity_response.Results, annuity_result)
+	}
+	return annuity_response
 }
--- backend/pkg/controllers/annuity_controller_test.go
+++ backend/pkg/controllers/annuity_controller_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
@@ -10,12 +11,12 @@ import (
 )
 
 type DummyLogger struct {
-	message string
+	Message string
 }
 
 func (l *DummyLogger) Info(v ...any) {
-	strConvert := fmt.Sprintf("%s", v)
-	l.message += strConvert
+	strConvert := fmt.Sprintf("%s", v[0])
+	l.Message += strConvert
 }
 
 func (l *DummyLogger) Debug(v ...any) {}
@@ -34,9 +35,24 @@ func TestCalculateAnnuityController(t *testing.T) {
 	r := gofight.New()
 
 	r.POST("/test").
-		// trun on the debug mode.
+		SetJSON(gofight.D{
+			"creditsum":                  350000,
+			"initial_repayment_rate":     2,
+			"interest_rate":              3,
+			"unscheduled_repayment_rate": 2,
+			"runtime":                    10,
+		}).
 		SetDebug(true).
 		Run(http.DefaultServeMux, func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+			var response AnnuityResponse
+			err := json.Unmarshal(r.Body.Bytes(), &response)
+
+			assert.Equal(t, nil, err)
+			assert.Equal(t, response.Results[4].Residual_dept, uint(293154))
+			assert.Equal(t, response.Results[4].Interest, uint(8795))
+			assert.Equal(t, response.Results[4].Repayment, uint(8705))
+			assert.Equal(t, response.Results[4].Unscheduled_repayment, uint(5863))
+			assert.Equal(t, response.Results[4].Annuity, uint(17500))
 			assert.Equal(t, http.StatusOK, r.Code)
 		})
 }
